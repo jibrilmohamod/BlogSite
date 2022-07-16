2716687c1a4bfe6d8aa900a02462e235b4862c67
Jibril Mohamod <jazimjibril@gmail.com>
2022-07-16 21:09:19 +0300

    Nav styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,9 @@
 <script setup>
- import { RouterLink, RouterView } from "vue-router"
+ import { RouterView } from "vue-router"
+ import TheNav from "./components/TheNav.vue"
 </script>
 
 <template>
- <div class="wrapper">
-  <nav class="w-full h-12 bg-red-500">
-   <RouterLink to="/">Home</RouterLink>
-   <RouterLink to="/about">About</RouterLink>
-  </nav>
- </div>
-
+ <TheNav></TheNav>
  <RouterView />
 </template>
---
+++ src/components/TheNav.vue
@@ -0,0 +1,19 @@
+<template>
+ <header
+  class="w-full h-20 bg-green-500 flex py-4 px-28 items-center justify-between"
+ >
+  <h1 class="text-3xl text-white border">VueBlog</h1>
+  <nav class="flex justify-self-end">
+   <router-link to="path" class="cursor-pointer border text-white px-7"
+    ><i class="fas fa-house inline text- border"></i> Home</router-link
+   >
+   <router-link to="path" class="border cursor-pointer text-white px-7"
+    >Login
+   </router-link>
+  </nav>
+ </header>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
