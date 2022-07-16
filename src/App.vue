2e522bbe2b777e38c7d33ae7506dcfdbf871a6e1
Jibril Mohamod <jazimjibril@gmail.com>
2022-07-16 14:40:15 +0300

    project init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+ import { RouterLink, RouterView } from "vue-router"
+</script>
+
+<template>
+ <div class="wrapper">
+  <nav class="w-full h-12 bg-red-500">
+   <RouterLink to="/">Home</RouterLink>
+   <RouterLink to="/about">About</RouterLink>
+  </nav>
+ </div>
+
+ <RouterView />
+</template>
