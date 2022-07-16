0c697c1b5563fb8d5da86c82598ed53814653933
Jibril Mohamod <jazimjibril@gmail.com>
2022-07-16 23:12:17 +0300

    registerpage

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,22 +1,27 @@
 <template>
  <header
-  class="lg:w-full h-20 bg-green-500 flex flex-col py-4 px-28 items-center justify-between md:flex-row"
+  class="lg:w-full h-[8vh] bg-green-500 flex flex-col py-4 px-28 items-center justify-between md:flex-row"
  >
   <h1 class="text-4xl text-white">VueBlog</h1>
   <nav class="flex flex-col justify-self-end md:flex-row">
    <router-link to="path" class="cursor-pointer text-white px-7"
-    ><i class="fas fa-house inline text-xl pr-1"></i> Home</router-link
+    ><i class="fas fa-house inline text-xl pr-1 hover:text-neutral-500"></i>
+    Home</router-link
    >
    <router-link to="path" class="cursor-pointer text-white px-7"
-    ><i class="text-xl fas fa-photo-film pr-1"></i>Categories
+    ><i class="text-xl fas fa-photo-film pr-1 hover:text-neutral-500"></i
+    >Categories
    </router-link>
 
    <router-link to="path" class="cursor-pointer text-white px-7"
-    ><i class="text-xl fas fa-user pr-1"></i>Me
+    ><i class="text-xl fas fa-user pr-1 hover:text-neutral-500"></i>Me
    </router-link>
 
    <router-link to="path" class="cursor-pointer text-white px-7"
-    ><i class="text-xl fas fas fa-person-walking-arrow-right pr-1"></i>Login
+    ><i
+     class="text-xl fas fas fa-person-walking-arrow-right pr-1 hover:text-neutral-500"
+    ></i
+    >Login
    </router-link>
   </nav>
  </header>
---
+++ src/views/TheRegister.vue
@@ -0,0 +1,9 @@
+<template>
+ <div class="bg-green-400 h-screen flex justify-center h-[92vh]">
+  <form class="border h-[69vh] w-[45vw] m-auto rounded-lg"></form>
+ </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
