4178d404a1c96919aab4590c502228a9d05779f0
Jibril Mohamod <jazimjibril@gmail.com>
2022-07-19 23:15:57 +0300

    register page done

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,7 +2,9 @@
  <header
   class="lg:w-full h-[8vh] bg-green-500 flex flex-col py-4 px-28 items-center justify-between md:flex-row"
  >
-  <h1 class="text-4xl text-white font-['Rubik_Moonrocks']">VueBlog</h1>
+  <router-link to="/" class="text-4xl text-white font-['Rubik_Moonrocks']"
+   >VueBlog</router-link
+  >
   <nav class="flex flex-col justify-self-end md:flex-row">
    <router-link to="path" class="cursor-pointer text-white px-7"
     ><i class="fas fa-house inline text-xl pr-1 hover:text-neutral-500"></i>
@@ -17,11 +19,11 @@
     ><i class="text-xl fas fa-user pr-1 hover:text-neutral-500"></i>Me
    </router-link>
 
-   <router-link to="path" class="cursor-pointer text-white px-7"
+   <router-link to="/register" class="cursor-pointer text-white px-7"
     ><i
      class="text-xl fas fas fa-person-walking-arrow-right pr-1 hover:text-neutral-500"
     ></i
-    >Login
+    >Sign Up
    </router-link>
   </nav>
  </header>
--- src/views/TheRegister.vue
+++ src/views/TheRegister.vue
@@ -2,17 +2,18 @@
  <div class="flex justify-center h-[92vh] fon">
   <form
    @submit.prevent="register"
-   class="h-[50vh] w-[50vw] m-auto rounded-xl bg-green-100 p-14 grid-cols-2 gap-2"
+   class="h-[50vh] w-[50vw] m-auto rounded-xl p-14 grid-cols-2 gap-2"
   >
    <!-- actual form  -->
-   <div class="row-start-1 row-span-2 border rounded-l-md flex flex-col gap-6">
-    <h1 class="text-2xl">Sign Up</h1>
+   <div class="row-start-1 row-span-2 rounded-l-md flex flex-col gap-6">
+    <h1 class="text-2xl font-bold">Sign Up</h1>
 
     <!-- name inputs -->
     <label for="username" class="absolute top-[25em]">
      <i class="fas fa-user text-sm"></i>
     </label>
     <input
+     required
      id="username"
      type="text"
      class="h-9 placeholder:focus:text-green-400 outline-none border-b border-black w-80 pl-7 text-sm"
@@ -60,22 +61,37 @@
      placeholder="Confirm Password"
      v-model="conf"
     />
+    <!-- submit button  -->
+    <button
+     type="submit"
+     class="place-self-start h-12 w-32 border hover:bg-green-400 hover:text-white"
+    >
+     Sign Up
+    </button>
    </div>
 
-   <div class="row-start-1 row-span-2 rounded-r-md border">02</div>
+   <!-- bgimage -->
+   <div class="row-start-1 row-span-2 rounded-r-md flex flex-col">
+    <div class="mx-auto">
+     <img src="@/assets/images/signup-image.jpg" class="h-72 w-72" alt="" />
+    </div>
+    <router-link to="path" class="m-auto border-b hover:border-black">
+     Already Registered?</router-link
+    >
+   </div>
   </form>
  </div>
 </template>
 
 <script setup>
  import { ref } from "vue"
- import { useRouter } from "vue-router"
+ //  import { useRouter } from "vue-router"
 
  const email = ref(null)
  const password = ref(null)
  const username = ref("")
  const conf = ref(null)
- const router = useRouter()
+ //  const router = useRouter()
 
  //  register function
 </script>
